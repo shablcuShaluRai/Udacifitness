33eeb01998939430a28668e2c7f76e3f5c198221
shalurai <shaluraics22@gmail.com>
2017-10-04 20:11:48 +0900

    Add app permission and animation

--- components/Live.js
+++ components/Live.js
@@ -1,21 +1,68 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
+import {Location, Permissions} from 'expo'
+import {calculateDirection} from '../utils/helpers'
 
 export default class Live extends Component {
   state = {
     coords: null,
-    status: 'granted',
-    direction: ''
+    status: null,
+    direction: '',
+    bounceValue: new Animated.Value(1)
   }
+  componentDidMount () {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
 
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => {
+        console.warn('Error getting Location permission: ', error)
+
+        this.setState(() => ({ status: 'undetermined' }))
+      })
+  }
   askPermission = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
 
-   }
+        this.setState(() => ({ status }))
+      })
+      .catch((error) => console.warn('error asking Location permission: ', error))
+  }
+  setLocation = () => {
+    Location.watchPositionAsync({
+      enableHighAccuracy: false,
+      timeInterval: 1,
+      distanceInterval: 1,
+    }, ({ coords }) => {
+      const newDirection = calculateDirection(coords.heading)
+      const { direction, bounceValue } = this.state
 
+      if (newDirection !== direction) {
+        Animated.sequence([
+          Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+          Animated.spring(bounceValue, { toValue: 1, friction: 4})
+        ]).start()
+      }
+
+      this.setState(() => ({
+        coords,
+        status: 'granted',
+        direction: newDirection,
+      }))
+    })
+  }
   render() {
-    const { status, coords, direction } = this.state
+    const { status, coords, direction, bounceValue } = this.state
 
     if (status === null) {
       return <ActivityIndicator style={{marginTop: 30}}/>
@@ -24,11 +71,11 @@ export default class Live extends Component {
     if (status === 'denied') {
       return (
         <View style={styles.center}>
-        <Foundation name='alert' size={50} />
+          <Foundation name='alert' size={50} />
           <Text>
             You denied your location. You can fix this by visiting your settings and enabling location services for this app.
           </Text>
-          </View>
+        </View>
       )
     }
 
@@ -50,11 +97,12 @@ export default class Live extends Component {
 
     return (
       <View style={styles.container}>
-      <View style={styles.directionContainer}>
-      <Text style={styles.header}>You're heading</Text>
-      <Text style={styles.direction}>
-             North
-          </Text>
+        <View style={styles.directionContainer}>
+          <Text style={styles.header}>You're heading</Text>
+          <Animated.Text
+          style={[styles.direction, {transform: [{scale: bounceValue}]}]}>
+                {direction}
+            </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
@@ -62,7 +110,7 @@ export default class Live extends Component {
               Altitude
             </Text>
             <Text style={[styles.subHeader, {color: white}]}>
-              {200} feet
+              {Math.round(coords.altitude * 3.2808)} Feet
             </Text>
           </View>
           <View style={styles.metric}>
@@ -70,70 +118,68 @@ export default class Live extends Component {
               Speed
             </Text>
             <Text style={[styles.subHeader, {color: white}]}>
-              {300} MPH
-          </Text>
+              {(coords.speed * 2.2369).toFixed(1)} MPH
+            </Text>
           </View>
         </View>
-        </View>
-      )
-    }
-
-    }
-
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        justifyContent: 'space-between'
-      },
-      center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginLeft: 30,
-        marginRight: 30,
-      },
-      button: {
-        padding: 10,
-        backgroundColor: purple,
-        alignSelf: 'center',
-        borderRadius: 5,
-        margin: 20,
-      },
-      buttonText :{
-        color: white,
-        fontSize: 20,
-      },
-      directionContainer: {
-        flex: 1,
-        justifyContent: 'center',
-      },
-      header: {
-        fontSize: 35,
-        textAlign: 'center',
-      },
-      direction: {
-        color: purple,
-        fontSize: 120,
-        textAlign: 'center',
-      },
-      metricContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        backgroundColor: purple,
-      },
-      metric: {
-        flex: 1,
-        paddingTop: 15,
-        paddingBottom: 15,
-        backgroundColor: 'rgba(255, 255, 255, 0.1)',
-        marginTop: 20,
-        marginBottom: 20,
-        marginLeft: 10,
-        marginRight: 10,
-      },
-      subHeader: {
-        fontSize: 25,
-        textAlign: 'center',
-        marginTop: 5,
-      },
-    })
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText :{
+    color: white,
+    fontSize: 20,
+  },
+  directionContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 35,
+    textAlign: 'center',
+  },
+  direction: {
+    color: purple,
+    fontSize: 120,
+    textAlign: 'center',
+  },
+  metricContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: purple,
+  },
+  metric: {
+    flex: 1,
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  subHeader: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 5,
+  },
+})
